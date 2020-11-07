37982c0d1528c521d603eb9a156b39be66d8c207
XDeviation <xth@LAPTOP-R1MR3AQT.localdomain>
2020-11-07 21:55:45 +0800

    label

--- code/read/input.py
+++ code/read/input.py
@@ -7,3 +7,7 @@ train_df = pd.read_csv(datapath, sep='\t', nrows=100)
 
 train_df['text_len'] = train_df['text'].apply(lambda x: len(x.split(' ')))
 print(train_df['text_len'].describe())
+
+plt.hist(train_df['text_len'], bins=200)
+plt.xlabel('a')
+plt.title('b')
